9ddae1b084116a956903cc34b055020c2556672d
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-03 20:47:21 +0530

    main page

---
+++ app/src/main/java/com/aiden/unscrambleappwithviewmodel/ui/GameFragment.kt
@@ -0,0 +1,30 @@
+package com.aiden.unscrambleappwithviewmodel.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GameFragment:Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
