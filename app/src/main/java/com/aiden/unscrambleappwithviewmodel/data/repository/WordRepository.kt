38ad6edc1a6287278a187f358997c7b71a6dbdd1
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-09 23:32:07 +0530

    repo added

---
+++ app/src/main/java/com/aiden/unscrambleappwithviewmodel/data/repository/WordRepository.kt
@@ -0,0 +1,14 @@
+package com.aiden.unscrambleappwithviewmodel.data.repository
+
+import com.aiden.unscrambleappwithviewmodel.data.api.Words
+
+class WordRepository {
+
+    private val wordsService = Words.createService()
+
+    suspend fun getWords(
+        offset: Int,
+        limit: Int,
+        wordsLength: Int
+    ) = wordsService.getWords(offset,wordsLength,limit)
+}
