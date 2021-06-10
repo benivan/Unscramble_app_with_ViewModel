b3b11b96860599d045eee63908538435ca1732fe
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-10 23:59:49 +0530

    new model

--- app/src/main/java/com/aiden/unscrambleappwithviewmodel/data/WordViewModel.kt
+++ app/src/main/java/com/aiden/unscrambleappwithviewmodel/data/WordViewModel.kt
@@ -1,5 +1,6 @@
 package com.aiden.unscrambleappwithviewmodel.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,18 +12,19 @@ import kotlinx.coroutines.launch
 class WordViewModel : ViewModel() {
 
     private val wordsRepository = WordRepository()
-
     private val _words = MutableLiveData<List<String>>(emptyList())
 
     val words: LiveData<List<String>>
         get() = _words
 
     private var round: Int = 1
-    private var offset: Int = round*100+1
-    private var limit: Int = 100
+    private var offset: Int = 0
 
     private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
+
+    private var _currentWord: MutableLiveData<String> = MutableLiveData("")
+    val currentWord: LiveData<String>
+        get() = _currentWord
 
 
     private var _score: MutableLiveData<Int> = MutableLiveData(0)
@@ -42,9 +44,11 @@ class WordViewModel : ViewModel() {
     ) {
         viewModelScope.launch {
             try {
-                val words1 = wordsRepository.getWords(offset, limit, wordsLength)
+                val words1 = wordsRepository.getWords(offset, wordsLength)
                 _words.value = words1.data.items
+//                Log.d("WordViewModel", words1.data.items.toString())
             } catch (e: Exception) {
+                e.printStackTrace()
                 _words.value = emptyList()
             }
         }
@@ -59,26 +63,26 @@ class WordViewModel : ViewModel() {
 
     fun getNextWord() {
         if (words.value!!.isNotEmpty()) {
-            currentWord = words.value!!.random()
+            _currentWord.value = words.value!!.random()
 
-            val tempWord = currentWord.toCharArray()
+            val tempWord = _currentWord.value!!.toCharArray()
             tempWord.shuffle()
 
-            while (String(tempWord) == currentWord) {
+            while (String(tempWord) == _currentWord.value) {
                 tempWord.shuffle()
             }
-            if (wordsList.contains(currentWord)) {
+            if (wordsList.contains(_currentWord.value)) {
                 getNextWord()
             } else {
                 _currentScrambledWord.value = String(tempWord)
                 _wordCount.value = _wordCount.value!! + 1
-                wordsList.add(currentWord)
+                wordsList.add(_currentWord.value!!)
             }
         }
     }
 
     fun isUserWordCorrect(playerWord: String): Boolean {
-        if (playerWord.equals(currentWord, true)) {
+        if (playerWord.equals(_currentWord.value, true)) {
             increaseScore()
             return true
         }
@@ -92,9 +96,15 @@ class WordViewModel : ViewModel() {
     fun reset() {
         _score.value = 0
         _wordCount.value = 0
+        _currentScrambledWord.value = "test"
+        offset = round * 100 + 1
         round += 1
-        wordsList = mutableListOf()
-        println(round)
+        _words.value = emptyList()
+    }
+
+
+    companion object {
+        private const val TAG = "WordViewModel"
     }
 
 
