b683fdfbf2b266825340c8e5b3078bede43117a9
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-25 02:09:17 +0530

    remove nav bar

--- app/src/main/java/com/aiden/unscrambleappwithviewmodel/MainActivity.kt
+++ app/src/main/java/com/aiden/unscrambleappwithviewmodel/MainActivity.kt
@@ -20,12 +20,12 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
+//        setupActionBarWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
 
 
 }
